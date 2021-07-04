526f69984a1c3f94ad40ad6dc415f2df181a436f
rafael <rafael-ownz@hotmail.com>
2021-07-04 03:46:25 -0300

    Initial commit

---
+++ src/main/java/ufrn/tads/eaj/matterconstrucao/MatterConstrucaoApplication.java
@@ -0,0 +1,13 @@
+package ufrn.tads.eaj.matterconstrucao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatterConstrucaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatterConstrucaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufrn/tads/eaj/matterconstrucao/MatterConstrucaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.tads.eaj.matterconstrucao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatterConstrucaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
